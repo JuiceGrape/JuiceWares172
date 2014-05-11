3e93dd26a8961a4518b909063aa8ac27c2a56db5
JuiceGrape <bmentjot@gmail.com>
2014-05-12 01:44:23 +0200

    getting ready for 2.1.1

--- src/main/java/com/juicegrape/juicewares/items/ItemRocketBoots.java
+++ src/main/java/com/juicegrape/juicewares/items/ItemRocketBoots.java
@@ -4,7 +4,6 @@ import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
