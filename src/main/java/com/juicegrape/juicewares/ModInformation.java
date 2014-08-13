039037a040edcb97b748c59c8e260dbcaf2b2db0
JuiceGrape <bmentjot@gmail.com>
2014-08-13 16:03:09 +0200

    Switched to forge 1180 and readied for release

--- src/main/java/com/juicegrape/juicewares/ModInformation.java
+++ src/main/java/com/juicegrape/juicewares/ModInformation.java
@@ -4,7 +4,7 @@ public class ModInformation {
 	
 	public static final String ID = "juicewares";
 	public static final String NAME = "JuiceWares";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final String CHANNEL = "juicewares";
 	
 }
